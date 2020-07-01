91e85391e2f83c03919a1d125bd6841b89535de8
Konstantinos <ga45buf@mytum.de>
2020-07-01 12:51:04 +0200

    Updated dependencies and  added better output in main kpm runner

--- R/kpm.R
+++ R/kpm.R
@@ -1,3 +1,5 @@
+kpm(graph_file = graph_file, indicator_matrices  = huntington_disease_up)
+
 #' Main method used for running KPM
 #'
 #' Runs KeyPathwayMiner localy via the standalone
@@ -12,15 +14,17 @@
 #' Use getNetworks('https://exbio.wzw.tum.de/keypathwayminer/')
 #' to see all networks.
 kpm <- function(indicator_matrices, graph_file = NULL) {
-    message(paste("Run type: ",kpm_options()$execution))
+    message(paste(">Run type: ",kpm_options()$execution))
 
     #### Prepare files ####
     files <- check_files(indicator_matrices, graph_file)
-    matrices_list <- files[[1]]
-    graph <- files[[2]]
+    indicator_matrices <- files[[1]]
+    graph_file <- files[[2]]
 
     #### Check parameters ####
-    check_parameters()
+    check_parameters(indicator_matrices)
+
+    to_java_arguments(indicator_matrices, graph_file)
 
     # #### Run KPM & Get results ####
     # if (kpm_options()$execution == "Remote") {
@@ -48,7 +52,7 @@ kpm <- function(indicator_matrices, graph_file = NULL) {
 #' Graph file: Null if no graph_file provided else path to graph_file.
 check_files <- function(indicator_matrices, graph_file){
     #### Check indicator_matrices ####
-    message("Checking files")
+    message(">Checking files")
     if(!is.null(indicator_matrices)){
         matrices <- list()
         # Before we start we check whether indicator_matrices consists only of one element
@@ -107,7 +111,7 @@ check_files <- function(indicator_matrices, graph_file){
                    "Valid input: a filepath, a data.frame or a list which can contain both.",
                    "For more information visit: https://exbio.wzw.tum.de/keypathwayminer/",sep = "\n"))
     }
-    message("Indicator matrices: √")
+    message("\tIndicator matrices: √")
     #### Check graph_file ####
     if(!is.null(graph_file)){
         if(is.character(graph_file)&(!file.exists(graph_file) | tools::file_ext(graph_file) != "sif")){
@@ -115,7 +119,7 @@ check_files <- function(indicator_matrices, graph_file){
                        "\nMake sure the graph_file is in sif format and has a .sif extension.",
                        "\nGiven filepath: ", graph_file))
         }
-    message("Graph file: √")
+    message("\tGraph file: √")
     } else if(is.null(graph_file) & kpm_options()$execution == "Local"){
         # In case a graph_file was not provide on a local run
         stop(paste("For local runs you must provide a graph_file.",
@@ -124,7 +128,7 @@ check_files <- function(indicator_matrices, graph_file){
     } else if (is.null(graph_file) & kpm_options()$execution == "Remote"){
         message("No graph file provided. Network will be selected from the web page using the graph_id.")
     }
-    message("File checks completed.")
+    message(">File checks completed")
     return(list(matrices, graph_file))
 }
 
@@ -134,43 +138,47 @@ check_files <- function(indicator_matrices, graph_file){
 #' as well as perturbation paramets. Checks sanity
 #' of parameters and if they are provided in the correct
 #' form.
-check_parameters <- function(){
+#'
+#'@param indicator_matrices List of paths to the indicator matrices.
+#'
+check_parameters <- function(indicator_matrices){
+    message(">Checking parameters")
 #### Check case exceptions parameter ####
     if(kpm_options()$use_range_l){
         # Batch run for l parameter
-        if(!(length(matrices) == length(kpm_options()$l_min) &
-             length(matrices) == length(kpm_options()$l_step) &
-             length(matrices) == length(kpm_options()$l_max))){
+        if(!(length(indicator_matrices) == length(kpm_options()$l_min) &
+             length(indicator_matrices) == length(kpm_options()$l_step) &
+             length(indicator_matrices) == length(kpm_options()$l_max))){
             stop("Number of matrices is not equal to the number of given l parameters.")
         }else if(!(class(kpm_options()$l_min) == "numeric" &
                    class(kpm_options()$l_step) == "numeric" &
                    class(kpm_options()$l_max) == "numeric")){
             stop("The parameters l_min, l_step and l_max must be numeric values or numeric vectors.")
         }
+        # Sanity check l parameters
+        l_min <- kpm_options()$l_min
+        l_step <- kpm_options()$l_step
+        l_max <- kpm_options()$l_max
+
+        case_1 <- l_max <= 0
+        case_2 <- l_min > l_max
+        case_3 <- l_step == 0
+        case_4 <- (l_max - l_step < l_min)
+
+        if(TRUE %in% case_1) stop("Configuration is incorrect: Invalid l_max")
+        if(TRUE %in% case_2) stop("Configuration is incorrect: Invalid l_min. It is larger than l_max.")
+        if(TRUE %in% case_3) stop("Configuration is incorrect: Invalid l_step. Must be larger than 0.")
+        if(TRUE %in% case_4) stop("Configuration is incorrect: Incrementation must be in range")
     } else if(!kpm_options()$use_range_l) {
         # Normal run for l parameter
-        if(!(length(matrices) == length(kpm_options()$l_min))){
+        if(!(length(indicator_matrices) == length(kpm_options()$l_min))){
             stop("Number of matrices is not equal to the number of given l parameters.")
         } else if(!(class(kpm_options()$l_min) == "numeric")){
             stop("The parameter l_min must be a numeric value or a numeric vector.")
         }
     }
 
-    # Sanity check l parameters
-    l_min <- kpm_options()$l_min
-    l_step <- kpm_options()$l_step
-    l_max <- kpm_options()$l_max
-
-    case_1 <- l_max <= 0
-    case_2 <- l_min > l_max
-    case_3 <- l_step == 0
-    case_4 <- (l_max - l_step < l_min)
-
-    if(TRUE %in% case_1) stop("Configuration is incorrect: Invalid l_max")
-    if(TRUE %in% case_2) stop("Configuration is incorrect: Invalid l_min. It is larger than l_max.")
-    if(TRUE %in% case_3) stop("Configuration is incorrect: Invalid l_step. Must be larger than 0.")
-    if(TRUE %in% case_4) stop("Configuration is incorrect: Incrementation must be in range")
-
+    message("\tCase exception parameters: √")
 #### Check gene exceptions parameter ####
 if(kpm_options()$algorithm == "INES"){
     if(kpm_options()$use_range_k){
@@ -186,6 +194,20 @@ if(kpm_options()$algorithm == "INES"){
             # In case one of the k_range parameters has not length 1
             stop("The parameters k_min, k_step and k_max must be all of length 1.")
         }
+        # Sanity check k-parameters
+        if (kpm_options()$k_max<= 0) stop(paste("Configuration is incorrect: ", "Invalid k_max", sep = ""))
+
+        if (kpm_options()$k_min > kpm_options()$k_max){
+            stop(paste("Configuration is incorrect: ", "Invalid k_min. It is larger than k_max", sep = ""))
+        }
+
+        if (kpm_options()$k_step == 0) {
+            stop(paste("Configuration is incorrect: ", "Invalid k_Step. Must be larger than 0", sep = ""))
+        }
+
+        if (kpm_options()$k_max - kpm_options()$k_step < pm_options()$k_min) {
+            stop(paste("Configuration is incorrect: ", "Invalid k_Step. Incrementation must be in range", sep = ""))
+        }
     } else if(!kpm_options()$use_range_k) {
         # For normal runs
         if(!(class(kpm_options()$k_min) == "numeric")){
@@ -194,22 +216,8 @@ if(kpm_options()$algorithm == "INES"){
             stop("The parameter k_min must be of length 1.")
         }
     }
-        }
-# Sanity check k-parameters
-    if (kpm_options()$k_max<= 0) stop(paste("Configuration is incorrect: ", "Invalid k_max", sep = ""))
-
-    if (kpm_options()$k_min > kpm_options()$k_max){
-        stop(paste("Configuration is incorrect: ", "Invalid k_min. It is larger than k_max", sep = ""))
-    }
-
-    if (kpm_options()$k_step == 0) {
-        stop(paste("Configuration is incorrect: ", "Invalid k_Step. Must be larger than 0", sep = ""))
-    }
-
-    if (kpmSettings.MAX_K - kpmSettings.INC_K < kpmSettings.MIN_K) {
-        stop(paste("Configuration is incorrect: ", "Invalid k_Step. Incrementation must be in range", sep = ""))
-    }
-
+}
+    message("\tGene exception parameters: √")
 #### Check perturbation parameters ####
 if(kpm_options()$with_perturbation){
     case_1 <- kpm_options()$perturbation_start <= 0
@@ -222,5 +230,7 @@ if(kpm_options()$with_perturbation){
     if(case_3) stop("Configuration is incorrect: Invalid l_step. Must be larger than 0.")
     if(case_4) stop("Configuration is incorrect: Incrementation must be in range")
 }
+    message("\tPerturbation parameters: √")
 
+    message(">Parameters checks complete")
 }

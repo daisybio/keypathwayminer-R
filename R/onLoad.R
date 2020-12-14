9f1e43edc8022270afa2be9e0b3c6a857c425c08
Konstantinos <ga45buf@mytum.de>
2020-12-15 00:24:00 +0100

    Added all_class File with all classes needed to make a the output of remote and local unified Updated Readmed and tested with higher java version than 1.8 -> works tests check now if version is higher than 1.8 Updated gitignor with results folder that is created when using the local execution

--- R/onLoad.R
+++ R/onLoad.R
@@ -5,20 +5,23 @@
   # Check if keypathwayminer standalone and core were added to clas path
   standalone <- FALSE
   for(i in 1:length(.jclassPath())){
-  if(grepl(pattern = "keypathwayminer-standalone-5.0.jar", x = .jclassPath()[i]) == TRUE){
+    if(grepl(pattern = "keypathwayminer-standalone-5.0.jar", x = .jclassPath()[i]) == TRUE){
       standalone <- TRUE
     }
   }
 
-  message(paste("Standalone jar added to class path: ",standalone))
+  message(paste("Standalone jar added to class path: ", standalone))
 
   if(standalone & test_jvm()){
-    # If jar is on the classpath and jvm is set to 1.8
-    message("KeyPathwayMineR ready for execution")
-  }else {
+    # If jar is on the classpath and jvm is greater than 1.8
+    message("KeyPathwayMineR ready for execution. Use following commands to get started:")
+    message("\t1. vignette(\"KeyPathwayMineR\")")
+    message("\t2. vignette(\"input_files_format\")")
+    message("\t3. ?kpm_options()")
+    }else {
     warning(paste("Local execution not possible at the moment.",
-                  "Visit https://exbio.wzw.tum.de/keypathwayminer/ for more information."
-                  ,sep = "\n"))
+                  "\nVisit https://github.com/baumbachlab/keypathwayminer-R for more information."
+                  , sep = ""))
   }
 
 }

400aefc118764006df938308b9bb99e64a8077eb
Konstantinos <ga45buf@mytum.de>
2021-01-13 13:40:56 +0100

    Added Z-score function A z-score function which calculates z-scores for the genes (or biological entities) in the case samples in contrast to the control samples.

--- R/utils.R
+++ R/utils.R
@@ -99,3 +99,28 @@ to_indicator_matrix <- function(numerical_matrix, operator = "<", threshold = 0.
     stop("Please provide a valid matrix.")
   }
 }
+
+#' Computes z-score values of case samples
+#'
+#' Computes z-scores of the genes in the case samples
+#' in contrast to the control samples.
+#'
+#' @param norm_counts Normalized counts as data.frame in TMM, TPM or FPKM.
+#' @param controls Vector with the column numbers of the control samples.
+#' @param cases Vector with the column numbers of the case samples.
+#'
+#' @return Data.frame with the z.score values of the case samples.
+#' @export
+#'
+#' @examples
+#' compute_z_scores(norm_counts, c(1, 2), c(3, 4))
+compute_z_scores <- function(norm_counts, controls, cases) {
+  # Compute means and standard_deviations from the control groups for every gene
+  gene_means <- rowMeans(norm_counts[, controls], na.rm = TRUE)
+  gene_standard_deviations <- apply(X = norm_counts[, controls], MARGIN = 1, FUN = sd, na.rm = TRUE)
+  # Compute z-score of case samples
+  z_score_matrix <- apply(X = norm_counts[, cases], MARGIN = 2, FUN = function(x) {
+    (x - gene_means) / gene_standard_deviations
+  })
+  return(z_score_matrix)
+}

3b7dfc813b6b6cb86d56c20d34f074307be61211
MichaelLauber <michael.lauber@tum.de>
2021-11-30 20:16:14 +0100

    added wrapper function profile_pathway()

---
+++ R/utils_DA.R
@@ -0,0 +1,26 @@
+#' Run gprofiler2's gost function on pathway of interesst
+#'
+#' @param result result object of kpm run
+#' @param configuration configuration of interest
+#' @param pathway pathway of interesst
+#' @param ... additional parameters which can be passed to gost function
+#'
+#' @return data.frame with all results from gost run
+#' @export
+#'
+#' @examples gost_pathway(result = local_example_1, configuration= "K-5-L1-2", pathway= "Pathway-1", organism = "hsapiens", numeric_ns = "ENTREZGENE_ACC")
+gost_pathway <- function(result, configuration, pathway = NULL, ...){
+
+  pathways_of_configuration <- get_pathways(result, configuration)
+
+  if(is.null(pathway)){
+    # extract all genes from a configuration
+    genesOfInteresst <- unlist(lapply(pathways_of_configuration, function(x){x@nodes$node}))
+  } else {
+    # Extract all genes from a pathway of interesst
+    pathway_of_interesst <- pathways_of_configuration[pathway]
+    genesOfInteresst <- pathway_of_interesst[[1]]@nodes$node
+  }
+
+  gprofiler2::gost(query = genesOfInteresst, ...)
+}
